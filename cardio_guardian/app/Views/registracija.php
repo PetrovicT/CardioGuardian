0eff6cba8c879b4812766d43ff5dc8515f5643d4
Teodora Petrovic <teodora.z.petrovic@gmail.com>
2021-06-15 01:57:42 +0200

    Dodat samo izgled logina i registracije

--- cardio_guardian/app/Views/registracija.php
+++ cardio_guardian/app/Views/registracija.php
@@ -14,6 +14,7 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open Sans">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
         <link rel="stylesheet" href="<?php echo base_url(); ?>/css/style.css">
+        <link rel="stylesheet" href="<?php echo base_url(); ?>/css/stil.css">
         <link rel="stylesheet" href="<?php echo base_url(); ?>/css/logovanje.css">
         <style>h1,h2,h3,h4,h5,h6 {font-family: "Oswald"} body {font-family: "Open Sans"}</style>
         <script src = "<?php echo base_url(); ?>/js/script.js"></script>
@@ -28,12 +29,13 @@
 
         <!-- Registracija -->
         <div class="logovanje">
-            <div class="wrapper">
+            <br> 
+            <div class="wrapper SlaboCrvenaPozadina regBox">
                 <div class="Naslov"><b>Formular za registrovanje</b></div>
+<br>
+                <div class="belaSlova ">Sledeca polja su obavezna, molimo Vas da ih popunite. Obratite paznju da Vasa sifra mora sadrzati minimum 6 karaktera.</div>
 
-                <div>Sledeca polja su obavezna, molimo Vas da ih popunite. Obratite paznju da Vasa sifra mora sadrzati minimum 6 karaktera.</div>
-
-                <font color='red'>
+                <font color='white'>
                 <?php
                 if (!empty($registrationErrorMessages)) {
                     foreach ($registrationErrorMessages as $msg) {
@@ -47,18 +49,20 @@
                     <div class="form-inner">
                         <form action="<?= site_url("Gost/registerSubmit") ?>" class="login" method="post">
                             <div class="field">
-                                <input name="username" type="text" placeholder="Korisnicko ime" >
+                                <input class="sivaPozadina" name="username" type="text" placeholder="Korisnicko ime" >
                             </div> 
                             <div class="field">
-                                <input name="password" type="password" placeholder="Sifra">
+                                <input class="sivaPozadina"  name="password" type="password" placeholder="Sifra">
                             </div> 
+                            <br>
                             <div class="field">
-                                <div>Sledeca polja nisu obavezna, tako da ih mozete ostaviti praznim. </div>
-                                <input name="licnoIme" type="text" placeholder="Licno ime" >
+                                <div class="belaSlova">Sledeca polja nisu obavezna, tako da ih mozete ostaviti praznim. </div>
+                                <input class="sivaPozadina" name="licnoIme" type="text" placeholder="Licno ime" >
                             </div> 
-                            <div class="field">
-                                <select class="select-input" name="grad" id="grad">
-                                    <option value="">Grad:</option>>
+                            <div class="field nopadding ">
+                         
+                                <select class="select-input sivaPozadina sivaSlova nopadding" name="grad" id="grad">
+                                    <option class="sivaPozadina nopadding" value="">Grad:</option>
                                     <?php
                                     // Dodati sve gradove kao opcije u alfabetnom poretku
                                     $gradModel = new \App\Models\GradModel();
@@ -67,16 +71,7 @@
                                         echo '<option value="' . $grad->idGrad . '">' . $grad->naziv . '</option>';
                                     }
 
-                                    /*
-                                      <input name = "grad" type="text" placeholder="Grad" list="gradovi" >
-                                     * 
-                                      <select name="cars" id="cars">
-                                      <option value="volvo">Volvo</option>
-                                      <option value="saab">Saab</option>
-                                      <option value="mercedes">Mercedes</option>
-                                      <option value="audi">Audi</option>
-                                      </select>
-                                     */
+                        
                                     ?>
                                 </select>
 
@@ -84,24 +79,26 @@
 
                             </div> 
                             <div class="field">
-                                <input name = "email" type="email" placeholder="Email adresa" >
+                                <input class="sivaPozadina" name = "email" type="email" placeholder="Email adresa" >
                             </div> 
 
-                            <div>Pol:</div>
+                            <br>
+                            <div class="belaSlova ">Pol:</div>
                             <?php
                             $polModel = new \App\Models\PolModel();
                             $sviPolovi = $polModel->findAll();
                             foreach ($sviPolovi as $pol) {
-                                echo '<input type="radio" id="' . $pol->pol . '" name="gender" value="' . $pol->idPol . '">';
-                                echo '<label for="' . $pol->pol . '">' . $pol->pol . '</label><br>';
+                                echo '<input class="belaSlova" type="radio" id="' . $pol->pol . '" name="gender" value="' . $pol->idPol . '">';
+                                echo '<label class="belaSlova" for="' . $pol->pol . '"> ' . $pol->pol . '</label><br>';
                             }
                             ?>
                             <input type="radio" id="Drugo" name="gender" value="" checked>
-                            <label for="Drugo">Drugo/Neizjašnjen</label>
+                            <label class="belaSlova " for="Drugo">Drugo/Neizjašnjen</label>
                             <div class="field">
                                 <input type="submit" value="Registrujte se">
                             </div>
-                            <div class="singup-link">
+                            <div class="singup-link belaSlova">
+                                
                                 Vec imate korisnicki nalog? <a href="<?php echo base_url(); ?>/Gost/login"><b>Ulogujte se</b></a>
                             </div>
                         </form>
